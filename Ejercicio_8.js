def9d815337a9dc078167ee1fa92f31da6d6e7b3
Krys-24 <cristiancobos24@gmail.com>
2024-12-15 23:10:30 +0100

    Delete Ejercicio_8.js

--- Ejercicio_8.js
+++
@@ -1,8 +0,0 @@
-console.log("De l'1 al 10:");
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
-}
-console.log("Nombres parells fins al 20:");
-for (let i = 2; i <= 20; i += 2) {
-    console.log(i);
-}
