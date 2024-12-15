0fd0ae0aef7b93350be5052701f01d22041e5503
Krys-24 <cristiancobos24@gmail.com>
2024-12-15 23:10:10 +0100

    Delete Ejercicio_4.js

--- Ejercicio_4.js
+++
@@ -1,6 +0,0 @@
-let dies = ["Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte", "Diumenge"];
-console.log("2n dia:", dies[1]);
-console.log("5é dia:", dies[4]);
-dies[1] = "Dia desconegut";
-dies[4] = "Dia desconegut";
-console.log("Vector modificat:", dies);
